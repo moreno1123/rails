2b8b987a7d2138819e30a4b8dbff7ce1cc62ed1d
Ubuntu <ubuntu@ip-172-31-39-99.ec2.internal>
2022-04-16 06:55:46 +0000

    prije usera

--- rails_app/config/routes.rb
+++ rails_app/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  get root to: 'pages#index'
+  get 'pages/contact'
+  get 'pages/about'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
