a0003b181ac791c820e574153a4542df1c36d9ee
Ubuntu <ubuntu@ip-172-31-39-99.ec2.internal>
2022-04-16 11:20:30 +0000

    articles

--- rails_app/config/routes.rb
+++ rails_app/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users,  :controllers => { registrations: 'registrations' }
+  resources :articles
   
   get root to: 'pages#index'
   get 'pages/contact'
   get 'pages/about'
   
-  devise_for :users,  :controllers => { registrations: 'registrations' }
 end
