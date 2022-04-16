8f9c8d28143ed79b1185f5501d1a8f15f8d58d5d
Ubuntu <ubuntu@ip-172-31-39-99.ec2.internal>
2022-04-16 15:01:39 +0000

    test

--- rails_app/config/routes.rb
+++ rails_app/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users,  :controllers => { registrations: 'registrations' }
-  resources :articles
+  resources :articles do 
+    resources :comments
+  end
   
   get root to: 'pages#index'
   get 'pages/contact'
--- rails_app/db/schema.rb
+++ rails_app/db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_16_112543) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_16_145229) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,5 +48,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_16_112543) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 
 end
